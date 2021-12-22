519b381dff8e3b27e38dc9e6e3cc5881f6c58951
jonathankim <ksyz12345@gmail.com>
2021-12-22 14:01:56 +0900

    비주얼 스튜디오 삼각형 넓이구하기

---
+++ sample1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	float a = 1.0f;
+	float b = 2.0f;
+
+	cin >> a;
+	cin >> b;
+
+	cout << "³ôÀÌ" << endl;
+	cout << "¹Øº¯" << endl;
+
+	float w = 3.0f;
+
+	w = (a * b) / 2;
+
+	cout << "³ÐÀÌ´Â" << w << "ÀÌ´Ù" << endl;
+		
+
+
+	return 0;
+
+
+}
